08e73f622f7680f8f522eec351981e19709c1580
Sesante1 <jomelsesante989@gmail.com>
2025-09-06 03:09:48 +0800

    added base config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    base: process.env.VITE_BASE_PATH || "/veehive-admin",
     server: {
         port: 3000,
     },
